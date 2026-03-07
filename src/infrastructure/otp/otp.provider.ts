cdc8acf2a8b59599d8c5cf148e04c803e9f55202
Shubham Khot <khots3636@gmail.com>
2026-03-07 22:57:03 +0530

    fix: stable JWT injection and mult

--- src/infrastructure/otp/otp.provider.ts
+++ src/infrastructure/otp/otp.provider.ts
@@ -1,30 +1,37 @@
-import axios from "axios";
-import logger from "../../core/logger";
+import axios from 'axios';
+import logger from '../../core/logger';
 
 export class OtpProvider {
 
   //////////////////////////////////////////////////////
-  // SEND OTP USING MSG91 DEFAULT OTP SERVICE
+  // SEND OTP USING MSG91
   //////////////////////////////////////////////////////
 
-  static async sendOTP(phoneNumber: string) {
+  static async sendSms(phoneNumber: string, otp: string) {
 
     try {
 
-      const response = await axios.get(
-        `https://control.msg91.com/api/v5/otp?authkey=${process.env.MSG91_AUTH_KEY}&mobile=91${phoneNumber}&otp_length=6`
-      );
+      const url =
+        `https://control.msg91.com/api/v5/otp?authkey=${process.env.MSG91_AUTH_KEY}&mobile=91${phoneNumber}&otp=${otp}&otp_length=6`;
+
+      const response = await axios.get(url);
+
+      logger.info({
+        event: 'OTP_SENT',
+        phoneNumber
+      });
 
       return response.data;
 
     } catch (error: any) {
 
       logger.error({
-        event: "MSG91_OTP_FAILED",
+        event: 'MSG91_OTP_FAILED',
+        phoneNumber,
         error: error?.response?.data || error.message
       });
 
-      throw new Error("Unable to send verification code");
+      throw new Error('Unable to send verification code');
 
     }
 
